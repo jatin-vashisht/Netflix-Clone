8d4c60a929b05b053bf0317a3e68f0591b252143
Jatin Vashisht <jatinvashisht11@gmail.com>
2024-01-25 13:23:08 +0530

    Authentication fix on Deployment

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,10 +14,10 @@ export const fetchToken = async () => {
 
         if(data.success){
             localStorage.setItem('request_token', token)
-            window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${window.location.origin}/approved`
+            window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${window.location.origin}`
         }
     } catch (error) {
-        console.log('Sorry, your token could not be created')       
+        console.log('Sorry, your token could not be created')
     }
 }
 
